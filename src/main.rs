6b62abd1b44b976562b3026dec6ac5ac08b8ef83
Feromond <Jacobpmish@gmail.com>
2023-05-25 09:41:24 -0600

    allows for any product id selection on bazzar

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,22 @@ use tokio::time::{interval, Duration};
 use crossterm::terminal::{self};
 use crossterm::ExecutableCommand;
 use tokio::signal;
-use futures::FutureExt;  // FutureExt provides the `.fuse()` method.
-
-use std::io::stdout;
+use futures::FutureExt;
+use std::io::{self, Write, stdout};
 use colored::*;
 use textplots::{Chart, Plot, Shape};
 
 #[derive(Deserialize, Debug)]
 struct ProductSummary {
     sell_summary: Vec<BuySellSummary>,
-    buy_summary: Vec<BuySellSummary>
+    buy_summary: Vec<BuySellSummary>,
 }
 
 #[derive(Deserialize, Debug)]
 struct BuySellSummary {
-    amount: u32,
+    // amount: u32,
     pricePerUnit: f64,
-    orders: u32,
+    // orders: u32,
 }
 
 #[derive(Deserialize, Debug)]
@@ -45,22 +44,21 @@ impl HistoricalData {
             time_min: 0.,
         }
     }
-    
+
     fn add(&mut self, buy_price: f32, sell_price: f32) {
         self.buy_prices.push((self.time, buy_price));
         self.sell_prices.push((self.time, sell_price));
         self.time += 1.;
-    
-        if self.buy_prices.len() > 100 {
+
+        if self.buy_prices.len() > 200 {
             self.buy_prices.remove(0);
             self.sell_prices.remove(0);
-            self.time_min +=1.;
+            self.time_min += 1.;
         }
     }
-    
 }
 
-async fn fetch_and_print(data: &mut HistoricalData) -> Result<(), Error> {
+async fn fetch_and_print(data: &mut HistoricalData, product_id: &str) -> Result<bool, Error> {
     let response = reqwest::get("https://api.hypixel.net/skyblock/bazaar")
         .await?
         .json::<BazaarResponse>()
@@ -68,29 +66,33 @@ async fn fetch_and_print(data: &mut HistoricalData) -> Result<(), Error> {
 
     stdout().execute(terminal::Clear(terminal::ClearType::All)).unwrap();
 
-    for (product_id, product) in response.products {
-        if product_id == "BOOSTER_COOKIE"{
+    let mut product_found = false;
+
+    for (product_key, product) in response.products {
+        if product_key == product_id {
+            product_found = true;
             let mut max_sell_price = 0.0;
             let mut min_sell_price = 99999999999.0;
             let mut max_buy_price = 0.0;
             let mut min_buy_price = 99999999999.0;
-            println!("{}", format!("Product ID: {}", product_id).yellow());
+
+            println!("{}", format!("Product ID: {}", product_key).yellow());
             for summary in product.sell_summary {
                 let current_amount:f64 = summary.pricePerUnit as f64;
-                if min_sell_price > current_amount{
-                    min_sell_price = current_amount
+                if min_sell_price > current_amount {
+                    min_sell_price = current_amount;
                 }
-                if max_sell_price < current_amount{
-                    max_sell_price = current_amount
+                if max_sell_price < current_amount {
+                    max_sell_price = current_amount;
                 }
             }
             for summary in product.buy_summary {
                 let current_amount:f64 = summary.pricePerUnit as f64;
-                if min_buy_price > current_amount{
-                    min_buy_price = current_amount
+                if min_buy_price > current_amount {
+                    min_buy_price = current_amount;
                 }
-                if max_buy_price < current_amount{
-                    max_buy_price = current_amount
+                if max_buy_price < current_amount {
+                    max_buy_price = current_amount;
                 }
             }
 
@@ -100,34 +102,59 @@ async fn fetch_and_print(data: &mut HistoricalData) -> Result<(), Error> {
             println!("Current Instant Sell Price: {}", format!("{}", max_sell_price).red());
             println!("Current Buy Order Price: {}", format!("{}", max_sell_price).bright_green());
             println!("Current Sell Order Price: {}", format!("{}", min_buy_price).bright_red());
-            
+
             println!("\nHistorical Buy Prices:");
-            Chart::new(240, 50, data.time_min, data.time).lineplot( Shape::Lines(&data.buy_prices) ).display();
+            Chart::new(220, 50, data.time_min, data.time).lineplot(Shape::Lines(&data.buy_prices)).display();
 
             println!("\nHistorical Sell Prices:");
-            Chart::new(240, 50, data.time_min, data.time).lineplot( Shape::Lines(&data.sell_prices) ).display();
+            Chart::new(220, 50, data.time_min, data.time).lineplot(Shape::Lines(&data.sell_prices)).display();
         }
     }
-    Ok(())
-}
 
+    if !product_found {
+        eprintln!("No product with id {} found", product_id);
+    }
+
+    Ok(product_found)
+}
 
 #[tokio::main]
 async fn main() {
     let mut stdout = stdout();
 
-    stdout
-        .execute(terminal::EnterAlternateScreen)
-        .expect("Failed to enter alternate screen");
+    stdout.execute(terminal::EnterAlternateScreen).expect("Failed to enter alternate screen");
 
-    let mut interval = interval(Duration::from_secs(15));
+    let mut product_id = String::new();
+    let mut product_found = false;
+
+    while !product_found {
+        print!("Enter the product id: ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut product_id).unwrap();
+
+        product_id = product_id.trim_end().to_uppercase().replace(" ", "_").replace("-","_");
+
+        let mut data = HistoricalData::new();
+        match fetch_and_print(&mut data, &product_id).await {
+            Ok(true) => product_found = true,
+            Ok(false) => {
+                eprintln!("No product with id {} found. Please try again.", product_id);
+                product_id = String::new();
+                continue;
+            },
+            Err(e) => eprintln!("Error: {}", e),
+        }
+    }
+
+    let mut interval = interval(Duration::from_secs(10));
     let mut data = HistoricalData::new();
 
     let ctrl_c = signal::ctrl_c().fuse();
+
     let main_loop = async {
         loop {
             interval.tick().await;
-            if let Err(e) = fetch_and_print(&mut data).await {
+            if let Err(e) = fetch_and_print(&mut data, &product_id).await {
                 eprintln!("Error: {}", e);
             }
         }
@@ -142,7 +169,6 @@ async fn main() {
         _ = &mut main_loop => { /* this will never be reached */ },
     }
 
-    stdout
-        .execute(terminal::LeaveAlternateScreen)
-        .expect("Failed to leave alternate screen");
+    stdout.execute(terminal::LeaveAlternateScreen).expect("Failed to leave alternate screen");
 }
+
